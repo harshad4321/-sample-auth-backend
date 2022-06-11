cb96e76dea2652652440b8e5f806692a4158feb8
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-06-11 17:56:51 +0530

    done customer more user friendaly

--- commands.js
+++ commands.js
@@ -1,21 +1,50 @@
 const program = require("commander");
-const {prompt}=require('inquirer')
+const {prompt}=require('inquirer');
 const {
     addCustomer,
     findCutomer
 } = require('./index');
 
+//Customer Questions
+const questions = [
+    
+ {   type:'input', 
+    name:'firstname',
+    message:'Customer First Name'},
+
+{
+    type:'input', 
+    name:'Lastname',
+    message:'Customer Last Name'},
+{
+    type:'input', 
+    name:'phone',
+    message:'Customer phone number'},
+{
+    type:'input', 
+    name:'email',
+    message:'Customer email Address'}
+]
+
 program
   .description('client Management System ')
   .version('0.8.0');
 
-  program
-  .command('add <firstname> <lastname> <phone> <email>')
-  .alias('a')
-  .description('Add a customer ')
-  .action((firstname ,lastname,phone,email)=>{
-    addCustomer({firstname,lastname,phone,email});
-  });
+//   program
+//   .command('add <firstname> <lastname> <phone> <email>')
+//   .alias('a')
+//   .description('Add a customer ')
+//   .action((firstname ,lastname,phone,email)=>{
+//     addCustomer({firstname,lastname,phone,email});
+//   });
+program 
+.command('add')
+.alias('a')
+.description('add a customer')
+.action(()=>{
+  prompt(questions).then(answers=> addCustomer(answers));
+})
+
   program
   .command('find <name>')
   .alias('f')
